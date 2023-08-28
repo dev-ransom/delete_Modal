294a3df3e31774312effb6e5049591dfae22b8cf
devransom-github <sundaystanley56@gmail.com>
2023-08-28 12:22:55 +0100

    added code for delete feature

---
+++ script.js
@@ -0,0 +1,9 @@
+const greenBtn = document.querySelector('#green_btn'), Modal = document.querySelector(".Modal"), textContent = document.querySelector('.text-content'), closeModal = document.querySelector('closeModal');
+
+greenBtn.addEventListener("click", ()=>{
+    textContent.style.display = "none"
+    Modal.style.display = 'flex';
+});
+
+closeModal.addEventListener("click", ()=)
+
