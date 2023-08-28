c0ee5552514febdaba2fd4468d3a9364ce567ca4
devransom-github <sundaystanley56@gmail.com>
2023-08-28 13:11:11 +0100

    added code for delete and cancel feature

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const greenBtn = document.querySelector('#green_btn'), Modal = document.querySelector(".Modal"), textContent = document.querySelector('.text-content'), closeModal = document.querySelector('closeModal');
+const greenBtn = document.querySelector('#green_btn'), Modal = document.querySelector(".Modal"), textContent = document.querySelector('.text-content'), closeModal = document.querySelector('.closeModal'), cancelBtn = document.querySelector('.cancelBtn'), deleteBtn = document.querySelector('.deleteBtn');
 
 greenBtn.addEventListener("click", ()=>{
-    textContent.style.display = "none"
+    textContent.style.display = "none";
     Modal.style.display = 'flex';
 });
 
-closeModal.addEventListener("click", ()=)
+let closeModalAndRestoreContent = () =>{
+    textContent.style.display = "block";
+    Modal.style.display = 'none';
+}
+
+cancelBtn.addEventListener('click', closeModalAndRestoreContent);
+deleteBtn.addEventListener('click', closeModalAndRestoreContent);
+closeModal.addEventListener('click', closeModalAndRestoreContent);
 
